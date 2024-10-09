7a5b54725743e8f3d64d84700e42c4a8666f69f7
John Fischer <john.h.fischer@comcast.net>
2024-10-09 16:03:58 -0700

    Update adds the us-west-1 provider

--- providers.tf
+++ providers.tf
@@ -1,14 +1,20 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "5.31.0"
+      source  = "hashicorp/aws"
+      version = ">= 5.31.0"
     }
   }
 }
 
 provider "aws" {
   region = "us-east-1"
-  profile = "Helen"
+  alias  = "east"
+  # Configuration options
+}
+
+provider "aws" {
+  region = "us-west-1"
+  alias  = "west"
   # Configuration options
 }
